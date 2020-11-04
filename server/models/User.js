2466fabb3cf55d88370eeca0ba2c59ecfdfc7d39
ldy2980 <ldyclank@gmail.com>
2020-11-04 20:10:16 +0900

    2020/11/04

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
-const saltRounds = 10
-const jwt = require("jsonwebtoken")
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -32,60 +32,62 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number
   }
-})
+});
 
-userSchema.pre("save", function (next) {
-  var user = this
+userSchema.pre('save', function (next) {
+  var user = this;
 
-  if (user.isModified("password")) {
+  if (user.isModified('password')) {
     //비밀번호 암호화
     bcrypt.genSalt(saltRounds, function (err, salt) {
-      if (err) return next(err)
+      if (err) return next(err);
 
       bcrypt.hash(user.password, salt, function (err, hash) {
-        if (err) return next(err)
-        user.password = hash
-        next()
-      })
-    })
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
   } else {
-    next()
+    next();
   }
-})
+});
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
   //암호화된 비밀번호와 동일한지 확인
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
-    if (err) return cb(err)
-    cb(null, isMatch)
-  })
-}
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 userSchema.methods.generateToken = function (cb) {
-  var user = this
-  //jsonwebtoken을 이용하여 token 생성
-  var token = jwt.sign(user._id.toHexString(), "secretToken")
-  user.token = token
-
+  var user = this;
+  // console.log('user._id', user._id)
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
+  user.token = token;
   user.save(function (err, user) {
-    if (err) return cb(err)
-    cb(null, user)
-  })
-}
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 userSchema.statics.findByToken = function (token, cb) {
-  var user = this
-
+  var user = this;
   // 토큰을 decode
-  jwt.verify(token, "secretToken", function (err, decoded) {
+  jwt.verify(token, 'secretToken', function (err, decoded) {
     // 유저 아이디를 이용하여 유저를 찾은 후
     // 클라이언트에서 가져온 토큰과 DB에 보관된 토큰이 일치하는지 확인
     user.findOne({ _id: decoded, token: token }, function (err, user) {
-      if (err) return cb(err)
-      cb(null, user)
-    })
-  })
-}
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
 
-const User = mongoose.model("User", userSchema)
-module.exports = { User }
+const User = mongoose.model('User', userSchema);
+module.exports = { User };
